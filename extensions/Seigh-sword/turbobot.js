233dafda8f65b0ae38d82cc93d93194870407526
Seigh sword <zack.yt.7085@gmail.com>
2026-01-15 14:18:17 +0530

    Update turbobot.js

--- extensions/Seigh-sword/turbobot.js
+++ extensions/Seigh-sword/turbobot.js
@@ -60,7 +60,7 @@
         id: "TurboBotAI",
         name: Scratch.translate("Turbo Bot"),
         sampleURL:
-          "https://turbowarp.org/editor?project_url=https://extensions.turbowarp.org/samples/Seigh-sword/Turbobot_sample.sb3",
+          "https://turbowarp.org/editor?project_url=https://extensions.turbowarp.org/samples/Turbobot_sample.sb3",
         docsURI: "https://docs.turbowarp.org/extensions/Seigh-sword/turbobot",
         menuIconURI: icon,
         blockIconURI: icon,
